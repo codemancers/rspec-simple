17cfd921c1084c663711d89f0af8aa8fa4f59524
Hemant Kumar <gethemant@gmail.com>
2014-04-15 07:40:17 -0400

    Push search related to rspec-simple repo

--- rspec-simple.el
+++ rspec-simple.el
@@ -35,6 +35,48 @@ of FILE in the current directory, suitable for creation"
                    (file-relative-name (buffer-file-name) (get-closest-gemfile-root))
                    ) t))
 
+;;; Code:
+(defun rspec-simple-shell-command (command file-separator working-dir)
+  "Executes 'command' and returns the list of printed files in
+   the form '((short/file/name . full/path/to/file) ...). The
+   'file-separator' character is used to split the file names
+   printed by the shell command and is usually set to \\n or \\0"
+  (let ((command-output (shell-command-to-string
+                         (format "cd %s; %s"
+                                 (shell-quote-argument working-dir) command))))
+    (let ((files (delete "" (split-string command-output file-separator))))
+      (mapcar (lambda (file)
+                (cons file (expand-file-name file working-dir)))
+              files))))
+
+(defun find-related-file ()
+  "find related file"
+  (interactive)
+  (let* (
+         (current-file-name (buffer-file-name))
+         (app-root (get-closest-gemfile-root))
+         (file-list (rspec-simple-shell-command (concat "search_related " current-file-name) "\n" app-root))
+         )
+    (rspec-simple-ido-find-file file-list)
+    ))
+
+(defun rspec-simple-ido-find-file (file-list)
+  "Actually find file to open, using ido."
+  (unwind-protect
+      (if (= 1 (length file-list))
+          (progn
+            (let (file (car (car file-list)))
+              (find-file (car (car file-list)))
+              ))
+        (progn
+          (let ((file (ido-completing-read "Related file "
+                                           (mapcar 'car file-list))))
+            (cond
+             (file (find-file (cdr (assoc file file-list))))
+             ((eq ido-exit 'fallback) (ido-find-file))))
+          ))))
+
+
 (add-hook 'enh-ruby-mode-hook
           (lambda ()
             (local-set-key (kbd "C-c l") 'rspec-compile-on-line)
